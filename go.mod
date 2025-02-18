26279ae6e4b6e1176aa4633f83eebf931d7195d4
Roberto Oliveros <robertooliveros.cg@outlook.com>
2025-02-18 06:04:40 -0600

    Add generics

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/roboli/adder
 
 go 1.24.0
+
+require golang.org/x/exp v0.0.0-20250215185904-eff6e970281f
--- lib.go
+++ lib.go
@@ -1,10 +1,18 @@
 // Package adds two numbers
 package adder
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Adds two numbers
 //
 // Expects two int values, returns sum with an int value
 // More: https://www.mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a T, b T) T {
 	return a + b
 }
