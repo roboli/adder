eb5e4ad0e5ce880ea7d1c351fd442741fc2f7dd9
Roberto Oliveros <robertooliveros.cg@outlook.com>
2025-02-18 05:54:25 -0600

    Add docs

--- lib.go
+++ lib.go
@@ -1,5 +1,10 @@
+// Package adds two numbers
 package adder
 
+// Adds two numbers
+//
+// Expects two int values, returns sum with an int value
+// More: https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
